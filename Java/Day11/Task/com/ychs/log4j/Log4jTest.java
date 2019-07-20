3642757add33b0c4617a62e266b55bf4dfa8ea47
xmy <xmy329@gmail.com>
2019-07-20 23:35:51 +0800

    log4jTest

---
+++ Java/Day11/Task/com/ychs/log4j/Log4jTest.java
@@ -0,0 +1,27 @@
+package com.ychs.log4j;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author Mingyu Xiong
+ * @description:
+ * @date 2019/07/19 15:47
+ */
+public class Log4jTest {
+    private static Logger logger = Logger.getLogger(Log4jTest.class);
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // System.out.println("This is println message.");
+
+        // 记录debug级别的信息
+        logger.debug("This is debug message.");
+        // 记录info级别的信息
+        logger.info("This is info message.");
+        // 记录error级别的信息
+        logger.error("This is error message.");
+    }
+
+}
