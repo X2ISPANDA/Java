fcbe919d9773d8d42f8a695dab1f6a97779a5497
xmy <xmy329@gmail.com>
2019-07-29 00:43:12 +0800

    Update

--- Java/Day11/Task/com/ychs/log4j/Log4jTest.java
+++ Java/Day11/Task/com/ychs/log4j/Log4jTest.java
@@ -1,6 +1,7 @@
 package com.ychs.log4j;
 
 import org.apache.log4j.Logger;
+import org.junit.jupiter.api.Test;
 
 /**
  * @author Mingyu Xiong
@@ -9,11 +10,11 @@ import org.apache.log4j.Logger;
  */
 public class Log4jTest {
     private static Logger logger = Logger.getLogger(Log4jTest.class);
-
     /**
      * @param args
      */
     public static void main(String[] args) {
+
 
         // System.out.println("This is println message.");
 
